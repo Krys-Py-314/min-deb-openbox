df9a7b5ab7251bb83e2efd31f089e6aca9d6601a
Krys-Py-314 <krys.py.ca@icloud.com>
2026-08-25 18:45:13 +0200

    Refactor font installation script and comment unused code
    Updated font installation process to handle errors and overwrite existing files. Commented out package installation and user configuration sections.

--- 10_openbox_terminal/install.sh
+++ 10_openbox_terminal/install.sh
@@ -16,20 +16,30 @@ echo "--------------------------------------------------------------------"
 echo -e "\e[1mInstalling packages...\e[0m"
 echo -e "\e[1mDoing nothing XTerm is enough...\e[0m"
 echo    " ..installing Nerd Fonts "
+
 mkdir -p ~/.local/share/fonts
+
 cd ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/0xProto.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/Inconsolata.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/Iosevka.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/IosevkaTerm.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/JetBrainsMono.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/UbuntuMono.zip
-unzip 0xProto.zip && rm 0xProto.zip
-unzip Inconsolata.zip && rm Inconsolata.zip
-unzip IosevkaTerm.zip && rm IosevkaTerm.zip
-unzip Iosevka.zip && rm Iosevka.zip
-unzip JetBrainsMono.zip && rm JetBrainsMono.zip
-unzip UbuntuMono.zip && rm UbuntuMono.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/0xProto.zip 2> /dev/null
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/Inconsolata.zip 2> /dev/null
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/Iosevka.zip 2> /dev/null
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/IosevkaTerm.zip 2> /dev/null
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/JetBrainsMono.zip 2> /dev/null
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.5.1/UbuntuMono.zip 2> /dev/null
+
+unzip -o 0xProto.zip
+ rm 0xProto.zip
+unzip -o Inconsolata.zip
+ rm Inconsolata.zip
+unzip -o IosevkaTerm.zip
+ rm IosevkaTerm.zip
+unzip -o Iosevka.zip
+ rm Iosevka.zip
+unzip -o JetBrainsMono.zip
+ rm JetBrainsMono.zip
+unzip -o UbuntuMono.zip
+ rm UbuntuMono.zip
 fc-cache -fv
 cd ~
 
@@ -52,21 +62,21 @@ EOF
 
 # Install packages
 #echo -e "\e[1mInstalling packages...\e[0m"
-#[ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
+#[ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update
 #apt-get install -y terminator
 
 # Copy users config
 #echo -e "\e[1mSetting configs to all users...\e[0m"
 #for d in /etc/skel/  /home/*/ /root; do
-#	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
+#	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes
 #
 #	# Create config folders if no exists
 #	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
-#	
+#
 #	f="helpers.rc"
 #	[ ! -d "$d/xfce4" ] && mkdir -v "$d/xfce4" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/xfce4"
 #	cp -v "$base_dir/$f" "$d/xfce4/" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/xfce4/$f"
-#	
+#
 #	d="$d/terminator/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
 #	f="config"
 #	cp -v "$base_dir/$f" "$d/" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/$f"
